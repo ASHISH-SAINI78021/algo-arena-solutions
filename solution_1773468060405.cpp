64a5a8e0b5ba2a43506bb7674b436f43e3add929
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-03-14 11:31:02 +0530

    valid-parenthesis

---
+++ solution_1773468060405.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.length();
+        stack<char> st;
+
+
+        for (int i = 0; i < n; i++){
+            if (s[i] == ')'){
+                if (st.empty() || st.top() != '(') return false;
+                st.pop();
+            }
+            else if (s[i] == ']'){
+                if (st.empty() || st.top() != '[') return false;
+                st.pop();
+            }
+            else if (s[i] == '}'){
+                if (st.empty() || st.top() != '{') return false;
+                st.pop();
+            }
+            else st.push(s[i]);
+        }
+
+        if (!st.empty()) return false;
+        return true;
+    }
+};
