1ec1e31ab862e4dc777bc2674164810d0969ca9b
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-03-19 21:08:34 +0530

    binary-subarrays-with-sums

---
+++ solution_1773934712344.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        int count = 0;
+        unordered_map<int, int> mp;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++){
+            sum += nums[i];
+            if (sum == goal) count++;
+            if (mp.find(sum - goal) != mp.end()){
+                count += mp[sum - goal];
+            }
+            mp[sum]++;
+        }
+
+        return count;
+    }
+};
