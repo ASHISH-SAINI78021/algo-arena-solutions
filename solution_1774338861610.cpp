bd197d10646c0a6143e5171eaead32f76279b3ff
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-03-24 13:24:22 +0530

    largest-rectangle-in-a-histogram

---
+++ solution_1774338861610.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> ple(n, -1);
+        vector<int> nle(n, n);
+        int maxArea = 0;
+        stack<int> st;
+
+        for (int i = 0; i < n; i++){
+            while (!st.empty() && heights[i] <= heights[st.top()]) st.pop();
+            if (!st.empty()) ple[i] = st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+        for (int i = n - 1; i >= 0; i--){
+            while (!st.empty() && heights[i] <= heights[st.top()]) st.pop();
+            if (!st.empty()) nle[i] = st.top();
+            st.push(i);
+        }
+
+        for (int i = 0; i < n; i++){
+            int width = nle[i] - ple[i] - 1;
+            int area = heights[i]*width;
+            maxArea = max(area, maxArea);
+        }
+
+        return maxArea;
+    }
+};
+
+
+// heights = 2 1 5 6 2 3
+// ple = -1 -1 1 2 1 4
+// nle =  1 6 4 4 6 6
+
+// ple[i] = 2
+// nle[i] = 4
+// nle[i] - ple[i] - 1
+// 4 - 2 - 1 = 1
