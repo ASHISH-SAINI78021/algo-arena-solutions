6a5e9542f541377df7714b6172d603887d40e203
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-04-09 23:01:16 +0530

    two-sum

---
+++ solution_1775755872835.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> ans;
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < n; i++){
+            if (mp.find(target - nums[i]) != mp.end()){
+                return {i, mp[target - nums[i]]};
+            }
+            mp[nums[i]] = i;
+        }
+
+        return {-1,-1};
+    }
+};
+
+
+// nums = 2 7 11 15
+// target = 9
+// ans -> 0 1
+// 2 + 7 = 9
+
+// Brute Force
+// T.C -> O(n*n)
+// S.C -> O(1)
+
+// Optimal Approach
+// T.C -> O(n)
+// S.C -> O(n);
+
+// a + b = target
+// b = target - a;
