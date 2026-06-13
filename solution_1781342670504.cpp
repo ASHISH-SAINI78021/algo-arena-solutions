01096ee2112a2e14511059775396d90448adfd7b
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-06-13 14:54:32 +0530

    Allocate Minimum Pages

---
+++ solution_1781342670504.cpp
@@ -0,0 +1,50 @@
+class Solution {
+  public:
+    bool isSafe(vector<int> &arr, int maxPages, int k){
+        int n = arr.size();
+        int count = 1;
+        int pages = 0;
+        for (int i = 0; i < n; i++){
+            if (pages + arr[i] <= maxPages){
+                pages += arr[i];
+            }
+            else {
+                count++;
+                pages = arr[i];
+            }
+        }
+        
+        return count <= k;
+    }
+    int findPages(vector<int> &arr, int k) {
+       int n = arr.size();
+       int start = *max_element(arr.begin(), arr.end());
+       int end = accumulate(arr.begin(), arr.end(), 0);
+       int ans = -1;
+       if (k > n) return -1;
+       while (start <= end){
+           int mid = start + (end - start)/2;
+           if (isSafe(arr, mid, k)){
+               ans = mid;
+               end = mid - 1;
+           }
+           else start = mid + 1;
+       }
+       
+       return ans;
+    }
+};
+
+
+
+
+
+// arr = 12 34 67 90    k = 2
+
+// task -> Number of students who can get books which have max pages of m
+// pages = min_element(12).......sum(n) => 
+
+// logic -> isSafe()
+// count = 1;
+// pages + arr[i] <= maxPages -> pages + arr[i] else count++;
+// count >= k
