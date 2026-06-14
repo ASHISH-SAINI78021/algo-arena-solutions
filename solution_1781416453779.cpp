86f2735a8e76342f9012ecb7dfa6a8c43a3ee0fd
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-06-14 11:24:15 +0530

    Group Anagrams

---
+++ solution_1781416453779.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        int n = strs.size();
+        unordered_map<string, vector<string>> mp;
+
+        for (int i = 0; i < n; i++){
+            string word = strs[i];
+            sort(word.begin(), word.end());
+            mp[word].push_back(strs[i]);
+        }
+
+        for (auto it : mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
+
+
+// Brute force
+// Time Complexity -> O(n*max(sizeofString)*log(maxSizeof string))
+// Space Complexity -> O(n)
+
+// <string, vector<string>> mp;
+// Time Complexity -> O(nklogk)
+// Space Complexity -> O(nk)
