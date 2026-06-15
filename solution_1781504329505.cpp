1f3e2926058bac03f4fe3be3382413e5ef21e53a
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-06-15 11:48:52 +0530

    Next greater element I

---
+++ solution_1781504329505.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        stack<int> st;
+        for (int i = m - 1; i >= 0; i--){
+            while (!st.empty() && nums2[i] >= st.top()){
+                st.pop();
+            }
+            if (!st.empty()) mp[nums2[i]] = st.top();
+            else mp[nums2[i]] = -1;
+            st.push(nums2[i]);
+        }
+
+        for (int i = 0; i < n; i++){
+            ans.push_back(mp[nums1[i]]);
+        }
+
+        return ans;
+    }
+};
