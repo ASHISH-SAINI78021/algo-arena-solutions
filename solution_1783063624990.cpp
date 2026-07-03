0a4eb7cb3bfa151761faa9f4c33d113e5d2af8f9
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-07-03 12:57:08 +0530

    Number of islands

---
+++ solution_1783063624990.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void dfs(int row, int col, vector<vector<char>> &grid, vector<vector<int>> &visited){
+        visited[row][col] = 1;
+        int arr[4][2] = {
+            {-1,0}, {0,1}, {1,0}, {0,-1}
+        };
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for (int i = 0; i < 4; i++){
+            int nrow = row + arr[i][0];
+            int ncol = col + arr[i][1];
+
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && visited[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
+                dfs(nrow, ncol, grid, visited);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (grid[i][j] == '1' && visited[i][j] == 0){
+                    dfs(i, j, grid, visited);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
