225f0a9fd77a1214418db03494eb1dbb7f937270
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-08-03 13:41:44 +0530

    Merge K sorted lists

---
+++ solution_1785744700914.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Compare {
+    public:
+    bool operator()(ListNode* &a, ListNode* &b){
+        return a->val > b->val;
+    }
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        if (n == 0) return NULL;
+        if (n == 1) return lists[0];
+
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+        for (int i = 0; i < lists.size(); i++){
+            if (lists[i] != NULL) pq.push(lists[i]);
+        }
+
+        while (!pq.empty()){
+            auto front = pq.top(); pq.pop();
+            curr->next = front;
+            curr = curr->next;
+            if (front->next) pq.push(front->next);
+        }
+
+        return dummy->next;
+    }
+};
+
+
+// Time Complexity : O(nlogk)
+// Space Complexity : O(k)
