ceebf942db1e5a738334af8963929f0300bdc2b3
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-08-11 14:07:40 +0530

    Two Sum

---
+++ Two Sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& arr, int target) {
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            if (arr[i] + arr[j] == target){
+                return {i, j};
+            }
+        }
+    }
+}
+
+int main() {
+    int n, target;
+    if (!(cin >> n >> target)) return 0;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    vector<int> res = twoSum(arr, target);
+    cout << res[0] << " " << res[1] << endl;
+    return 0;
+}
