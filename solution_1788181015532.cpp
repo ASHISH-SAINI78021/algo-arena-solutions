3877103e61f3bb7595a06a9d10b6b80dddfa9eba
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-08-31 18:26:58 +0530

    Course Schedule II

---
+++ solution_1788181015532.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& edges) {
+        vector<int> ans;
+        vector<vector<int>> adj(V);
+        vector<int> indegree(V, 0);
+        queue<int> q;
+
+        for (int i = 0; i < edges.size(); i++){
+            int v = edges[i][0];
+            int u = edges[i][1];
+            adj[u].push_back(v);
+        }
+
+        for (int i = 0; i < V; i++){
+            for (auto it : adj[i]) indegree[it]++;
+        }
+
+        for (int i = 0; i < V; i++){
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        while (!q.empty()){
+            auto front = q.front(); q.pop();
+            ans.push_back(front);
+            for (auto it : adj[front]){
+                indegree[it]--;
+                if (indegree[it] == 0) q.push(it);
+            }
+        }
+
+        if (ans.size() != V) return {};
+        return ans;
+    }
+};
