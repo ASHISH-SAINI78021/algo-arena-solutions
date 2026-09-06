cf4e1d4263b37ba62fc7b9f55d3fedb5be66feec
Ashish Saini <138544202+ASHISH-SAINI78021@users.noreply.github.com>
2026-09-06 17:13:42 +0530

    Binary Tree Maximum Path Sum

---
+++ solution_1788695020186.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxi = INT_MIN;
+    int solve(TreeNode* root){
+        if (root == NULL) return 0;
+        int ansleft = max(0, solve(root->left));
+        int ansright = max(0, solve(root->right));
+        maxi = max(maxi, ansleft + ansright + root->val);
+
+        return max(ansleft, ansright) + root->val;
+    }
+    int maxPathSum(TreeNode* root) {
+        if (root == NULL) return 0;
+        solve(root);
+        return maxi;
+    }
+};
